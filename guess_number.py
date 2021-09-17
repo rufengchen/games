2009282a5230c4f8ebbd582a436d454b2843b266
rufengchen <rrchen6-c@my.cityu.edu.hk>
2021-09-18 00:31:47 +0800

    first time write

---
+++ guess_number.py
@@ -0,0 +1,21 @@
+# Let the computer guess the integer number you have
+import random
+
+
+def guess_num(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback !='c':
+        if low == high:
+            guess = high  # low = high
+        else:
+            guess = random.randint(low,high)
+        if feedback == 'h':
+            high = guess -1
+            print("Please guess again!")
+        elif feedback == 'l':
+            low = guess-1
+            print("Please guess again!")
+    print("Here is your number: {0}".format(guess))
+guess_num(2)
