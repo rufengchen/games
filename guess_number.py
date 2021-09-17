1ce3da8d87b1da900348b506ca21af3836606dce
rufengchen <rrchen6-c@my.cityu.edu.hk>
2021-09-18 00:41:09 +0800

    final version(run successfully)

--- guess_number.py
+++ guess_number.py
@@ -1,7 +1,5 @@
 # Let the computer guess the integer number you have
 import random
-
-
 def guess_num(x):
     low = 1
     high = x
@@ -11,11 +9,12 @@ def guess_num(x):
             guess = high  # low = high
         else:
             guess = random.randint(low,high)
+        feedback = input("Is {0} too high(h), too low(l), or correct (c)?".format(guess)).lower()
         if feedback == 'h':
             high = guess -1
             print("Please guess again!")
         elif feedback == 'l':
-            low = guess-1
+            low = guess + 1
             print("Please guess again!")
     print("Here is your number: {0}".format(guess))
 guess_num(2)
