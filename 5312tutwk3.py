c0d2a4bf3ef9b280a6611aa32f5c7172b00d1a61
rufengchen <rrchen6-c@my.cityu.edu.hk>
2021-09-22 23:29:36 +0800

    tut wk3_1.5

--- 5312tutwk3.py
+++ 5312tutwk3.py
@@ -31,3 +31,34 @@ new_list_1_3 = []
 for m in months:
     new_list_1_3.append(m[0:3])
 print(new_list_1_3)
+
+
+# WK3 1.4 Create a Menu
+print("Enter a number from the menu to obtain a fact:")
+print("1: Where is the heart of a shrimp located?")
+print("2: What is the only animal that can't jump?")
+print("3: For an ostrich, is the eye bigger than its brain?")
+print("4: What is the English word with the most defintions?")
+print("5: Quit")
+
+
+while True:
+    selection = eval(input("Make a selection from the menu: "))
+    if selection == 1:
+        print("Head")
+    elif selection == 2:
+        print("Elephant")
+    elif selection == 3:
+        print("Ostrich's eye is bigger than it's brain")
+    elif selection == 4:
+        print("Set")
+    else:
+        print("Thank you for using our quiz! Bye")
+        break
+# WK3 1.5
+# Password validation
+while True:
+    password = input("Please enter password: ")
+    if len(password)>=6 and password.isalnum():
+        print("Password set successfully")
+        break
